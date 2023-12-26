179deee2e59ec5a11f30809eb6671aca34ce4d91
rayhanhossen <to.rayhanhossen@gmail.com>
2023-12-27 00:07:54 +0600

    feat: fetch external api data

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
+import axios from 'axios';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+  async searchPost(keyword: string): Promise<Post[] | Error> {
+    try {
+      // Fetch external API data
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      if (response.status !== 200) return new Error("Failed to fetch data");
+      const postData = response.data;
 
-  findAll() {
-    return `This action returns all post`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
-
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+      // Filter posts based on the keyword
+      const filteredPosts = postData.filter((item: Post) => item.title.includes(keyword) || item.body.includes(keyword));
+      console.log(filteredPosts)
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
