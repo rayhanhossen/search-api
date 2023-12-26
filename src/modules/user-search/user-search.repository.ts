8e3ef28b9675ddc1c9f5dd1da1f8633a35a5d828
rayhanhossen <to.rayhanhossen@gmail.com>
2023-12-27 00:42:59 +0600

    Create createEntity method in userSearch repository

--- src/modules/user-search/user-search.repository.ts
+++ src/modules/user-search/user-search.repository.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { UserSearch } from "./entities/user-search.entity";
+import { CreateUserSearchDto } from "./dto/create-user-search.dto";
 
 @Injectable()
 export class PostRepository extends Repository<UserSearch> {
     constructor(private readonly dataSource: DataSource) {
         super(UserSearch, dataSource.createEntityManager());
+    }
+
+    async createEntity(dto: CreateUserSearchDto): Promise<UserSearch | Error> {
+        try {
+            const entity = this.create(dto);
+            await this.save(entity);
+            return entity;
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
