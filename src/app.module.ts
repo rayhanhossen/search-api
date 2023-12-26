7a6375435a192ef6ef871b074c2d6d27de22b309
rayhanhossen <to.rayhanhossen@gmail.com>
2023-12-26 22:33:59 +0600

    Setup mysql database connection and configure dotenv globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
 import { UserSearchModule } from './modules/user-search/user-search.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostModule, UserSearchModule],
+  imports: [
+    // Configure the dotenv globlally 
+    ConfigModule.forRoot({ isGlobal: true }),
+    // MySQL database connection 
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      // username: process.env.USERNAME,
+      username: 'root',
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      dropSchema: false
+    }),
+    // Import the module of posts and users search
+    PostModule,
+    UserSearchModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
